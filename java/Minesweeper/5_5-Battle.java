22f19992ba34038ab072c65ec5c42a8266c48690
MatthewYung0 <65487408+MatthewYung0@users.noreply.github.com>
2022-06-13 11:35:00 +0200

    Update 5_5-Battle.java

--- java/Minesweeper/5_5-Battle.java
+++ java/Minesweeper/5_5-Battle.java
@@ -5,11 +5,13 @@ import java.util.Random;
 
 public class Main {
 
-    static Scanner scanner = new Scanner(System.in);
     final static int maxColumns = 9;
     final static int maxRows = 9;
     static int currentColumn = 0;
     static int currentRow = 0;
+    static char[][] revealedMinefield = new char[maxColumns][maxRows];
+    static int foundMines = 0;
+    static int cellCount = maxColumns * maxRows;
 
     public static void main(String[] args) {
         run();
@@ -17,23 +19,157 @@ public class Main {
 
     public static void run() {
 
+        // Code to create a copy of the mines
         int numberOfMines = getNumberOfMines();
+        revealedMinefield = createEmptyMinefield();
+        addMinesToMinefield(revealedMinefield, numberOfMines);
+        scanAndChangeMineField(revealedMinefield);
+        //printMinefield(revealedMinefield); // Code to print positions of mines
 
-        char[][] minefield = createEmptyMinefield();
-        printMinefield(minefield);
+        char[][] userMinefield = createEmptyMinefield();
 
-        System.out.println("Number of Mines: " + numberOfMines);
+        while (foundMines < numberOfMines && cellCount != numberOfMines) {
 
-        String input = getUserInput();
-        setColumnValue(input);
-        setRowValue(input);
+            printMinefield(userMinefield);
+            String input = getUserInput();
+            String command = getCommand(input);
+            setColumnValue(input);
+            setRowValue(input);
 
-        System.out.print(currentColumn + " " + currentRow + " " + getCommand(input));
+            if (command.equals("free")) {
+                if (revealedMinefield[currentRow][currentColumn] == STATES.MINE.getState()) {
+                    revealMines(userMinefield);
+                    printMinefield(userMinefield);
+                    System.out.println("You stepped on a mine and failed!");
+                    break;
+                } else if (revealedMinefield[currentRow][currentColumn] == STATES.HIDDEN.getState()){
+                    floodFill(userMinefield, new boolean[maxRows][maxColumns], currentRow, currentColumn);
+                } else {
+                    userMinefield[currentRow][currentColumn] = revealedMinefield[currentRow][currentColumn];
+                    cellCount--;
+                }
+            } else if (command.equals("mine")) {
+                if (revealedMinefield[currentRow][currentColumn] == STATES.MINE.getState()) {
+                    if (userMinefield[currentRow][currentColumn] != STATES.FLAGGED.getState()) {
+                        userMinefield[currentRow][currentColumn] = STATES.FLAGGED.getState();
+                        foundMines++;
+                    } else if (userMinefield[currentRow][currentColumn] == STATES.FLAGGED.getState()) {
+                        userMinefield[currentRow][currentColumn] = STATES.HIDDEN.getState();
+                        foundMines--;
+                    }
+                    // When cell is not mined...
+                } else if (revealedMinefield[currentRow][currentColumn] != STATES.MINE.getState()) {
+                    // ...and userMinefield is not
+                    if (userMinefield[currentRow][currentColumn] != STATES.FLAGGED.getState()) {
+                        userMinefield[currentRow][currentColumn] = STATES.FLAGGED.getState();
+                        foundMines--;
+                        cellCount++;
+                    } else if (userMinefield[currentRow][currentColumn] == STATES.FLAGGED.getState()) {
+                        userMinefield[currentRow][currentColumn] = STATES.HIDDEN.getState();
+                        foundMines++;
+                        cellCount--;
+                    }
+                }
+            }
+        }
+        printMinefield(userMinefield);
+        System.out.println("Congratulations! You found all the mines!");
+    }
 
-        addMinesToMinefield(minefield, numberOfMines);
+    public static void floodFill(char[][] userMinefield, boolean[][] visited, int currentRow, int currentColumn) {
+        // quit if off the grid:
+        if(currentRow < 0 || currentRow >= revealedMinefield.length || currentColumn < 0 || currentColumn >= revealedMinefield[0].length) return;
 
-        printMinefield(minefield);
+        // quit if visited:
+        if(visited[currentRow][currentColumn]) return;
+        visited[currentRow][currentColumn] = true;
 
+        // quit if hit boundaries:
+        if((revealedMinefield[currentRow][currentColumn] == STATES.NEARBY_1.getState() && userMinefield[currentRow][currentColumn] != STATES.NEARBY_1.getState()) ||
+                (revealedMinefield[currentRow][currentColumn] == STATES.NEARBY_2.getState() && userMinefield[currentRow][currentColumn] != STATES.NEARBY_2.getState()) ||
+                (revealedMinefield[currentRow][currentColumn] == STATES.NEARBY_3.getState() && userMinefield[currentRow][currentColumn] != STATES.NEARBY_3.getState()) ||
+                (revealedMinefield[currentRow][currentColumn] == STATES.NEARBY_4.getState() && userMinefield[currentRow][currentColumn] != STATES.NEARBY_4.getState()) ||
+                (revealedMinefield[currentRow][currentColumn] == STATES.NEARBY_5.getState() && userMinefield[currentRow][currentColumn] != STATES.NEARBY_5.getState()) ||
+                (revealedMinefield[currentRow][currentColumn] == STATES.NEARBY_6.getState() && userMinefield[currentRow][currentColumn] != STATES.NEARBY_6.getState()) ||
+                (revealedMinefield[currentRow][currentColumn] == STATES.NEARBY_7.getState() && userMinefield[currentRow][currentColumn] != STATES.NEARBY_7.getState()) ||
+                (revealedMinefield[currentRow][currentColumn] == STATES.NEARBY_8.getState() && userMinefield[currentRow][currentColumn] != STATES.NEARBY_8.getState()) ||
+                (revealedMinefield[currentRow][currentColumn] == STATES.UNHIDDEN.getState() && userMinefield[currentRow][currentColumn] != STATES.UNHIDDEN.getState()))
+        {
+            userMinefield[currentRow][currentColumn] = revealedMinefield[currentRow][currentColumn];
+            cellCount--;
+
+            return;
+        } else if (revealedMinefield[currentRow][currentColumn] == STATES.FLAGGED.getState()) {
+            return;
+        }
+
+        // we want to visit places with periods in them:
+        if(revealedMinefield[currentRow][currentColumn] == STATES.HIDDEN.getState() && userMinefield[currentRow][currentColumn] != STATES.UNHIDDEN.getState()) {
+            userMinefield[currentRow][currentColumn] = STATES.UNHIDDEN.getState();
+            cellCount--;
+        }
+
+        // recursively fill in all directions
+        floodFill(userMinefield,visited,currentRow+1,currentColumn);
+        floodFill(userMinefield,visited,currentRow-1,currentColumn);
+        floodFill(userMinefield,visited,currentRow,currentColumn+1);
+        floodFill(userMinefield,visited,currentRow,currentColumn-1);
+        floodFill(userMinefield,visited,currentRow-1,currentColumn-1);
+        floodFill(userMinefield,visited,currentRow+1,currentColumn+1);
+        floodFill(userMinefield,visited,currentRow-1,currentColumn+1);
+        floodFill(userMinefield,visited,currentRow+1,currentColumn-1);
+    }
+
+    public static void revealMines(char[][] minefield) {
+        for (int column = 0; column < maxColumns; column++) {
+            for (int row = 0; row < maxRows; row++) {
+                if (revealedMinefield[row][column] == STATES.MINE.getState()) {
+                    minefield[row][column] = STATES.MINE.getState();
+                }
+            }
+        }
+    }
+
+    public static void scanAndChangeMineField(char[][] minefield) {
+        for (int rowPos = 0; rowPos < maxRows; rowPos++) {
+            for (int columnPos = 0; columnPos < maxColumns; columnPos++) {
+                // if statement for checking all around the cell if it is mined.
+                if (minefield[rowPos][columnPos] == STATES.MINE.getState()) {
+                    for (int row = - 1; row < 2; row ++) {
+                        for (int column = -1; column < 2; column++) {
+                            // if cell is out of bounds, ignore and continue with loop
+                            try {
+                                // change the state of the points in the points
+                                minefield[rowPos + row][columnPos + column] = changeCellState(minefield[rowPos + row][columnPos + column]);
+                            } catch (ArrayIndexOutOfBoundsException ignored) { }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static char changeCellState(char state) {
+        switch(state) {
+            case '.':
+                return '1';
+            case '1':
+                return '2';
+            case '2':
+                return '3';
+            case '3':
+                return '4';
+            case '4':
+                return '5';
+            case '5':
+                return '6';
+            case '6':
+                return '7';
+            case '7':
+                return '8';
+            default:
+                return state;
+        }
     }
 
     public static void addMinesToMinefield(char[][] minefield, int numberOfMines) {
@@ -75,7 +211,7 @@ public class Main {
     }
 
     public static String getUserInput() {
-        scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Set/unset mines marks or claim a cell as free: ");
         return scanner.nextLine();
     }
@@ -98,28 +234,29 @@ public class Main {
             System.out.print(columnNo);
         }
         System.out.println("|");
-        System.out.print("-|");
-        for (int i = 1; i <= maxColumns; i++) {
-            System.out.print("-");
-        }
-        System.out.println("|");
+        printLines();
         for (int rowIndex = 0; rowIndex < maxRows; rowIndex++) {
-            System.out.printf((rowIndex + 1) + "|");
+            System.out.print((rowIndex + 1) + "|");
             for (int columnIndex = 0; columnIndex < maxColumns; columnIndex++) {
                 System.out.print(minefield[rowIndex][columnIndex]);
             }
             System.out.print("|" + '\n');
         }
-        System.out.print("-|");
-        for (int i = 1; i <= maxColumns; i++) {
-            System.out.print("-");
-        }
-        System.out.println("|");
+        printLines();
     }
 
     public static int getNumberOfMines() {
+        Scanner scanner = new Scanner(System.in);
         System.out.print("How many mines do you want on the field? ");
         return scanner.nextInt();
+    }
+
+    public static void printLines() {
+        System.out.print("-|");
+        for (int rowNo = 1; rowNo <= maxColumns; rowNo++) {
+            System.out.print("-");
+        }
+        System.out.println("|");
     }
 
 }
