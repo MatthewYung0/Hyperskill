cf560c57d4d6d7fc22b214076859c6ffca96218f
MatthewYung0 <65487408+MatthewYung0@users.noreply.github.com>
2023-01-28 22:38:07 +0100

    Create Main.java

---
+++ java/Contacts/2_4-CreateAMenu/Main.java
@@ -0,0 +1,10 @@
+package contacts;
+
+public class Main {
+    public static void main(String[] args) {
+        Contact newContact = new Contact();
+
+        System.out.println("\nA record created!");
+        System.out.println("A Phone Book with a single record created!");
+    }
+}
