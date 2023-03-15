fb41244e27de73a5ffcd542e6cf6a400d83e7e9a
MatthewYung0 <65487408+MatthewYung0@users.noreply.github.com>
2023-03-15 18:08:55 +0100

    Delete java directory

--- java/JSONDatabase/1_6-100CellDatabase/COMMANDS.java
+++
@@ -1,6 +0,0 @@
-package common;
-public enum COMMANDS {
-    SET,
-    GET,
-    DELETE;
-}
--- java/TicTacToeWithAI/4_5-SignsOfIntelligence/User.java
+++
@@ -1,7 +0,0 @@
-package tictactoe;
-
-import java.util.List;
-
-public abstract class User {
-    abstract public void run(List<Character> gameField);
-}
