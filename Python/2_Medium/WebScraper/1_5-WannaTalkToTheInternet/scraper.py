2d09f03a323f09d24bf3add4a9ae2fa0c48a0a25
MatthewYung0 <65487408+MatthewYung0@users.noreply.github.com>
2023-04-24 20:38:23 +0200

    Create scraper.py

---
+++ Python/2_Medium/WebScraper/1_5-WannaTalkToTheInternet/scraper.py
@@ -0,0 +1,14 @@
+import requests
+
+
+def main():
+    print("Input the URL:")
+    link = input()
+    response = requests.get(link)
+    try:
+        print(response.json()['content'])
+    except KeyError:
+        print("Invalid quote resource")
+
+
+main()
