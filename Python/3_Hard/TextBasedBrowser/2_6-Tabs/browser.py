934d8febb7bb2484688a57aa29317e1785480bc9
MatthewYung0 <65487408+MatthewYung0@users.noreply.github.com>
2023-07-28 20:49:13 +0200

    Update browser.py

--- Python/3_Hard/TextBasedBrowser/2_6-Tabs/browser.py
+++ Python/3_Hard/TextBasedBrowser/2_6-Tabs/browser.py
@@ -1,7 +1,5 @@
 import sys
 import os
-from pathlib import Path
-from os.path import exists
 
 nytimes_com = '''
 This New Liquid Is Magnetic, and Mesmerizing
@@ -14,8 +12,8 @@ and robotics. (Source: New York Times)
 Most Wikipedia Profiles Are of Men. This Scientist Is Changing That.
 
 Jessica Wade has added nearly 700 Wikipedia biographies for
- important female and minority scientists in less than two 
- years.
+important female and minority scientists in less than two 
+years.
 
 '''
 
@@ -23,25 +21,30 @@ bloomberg_com = '''
 The Space Race: From Apollo 11 to Elon Musk
 
 It's 50 years since the world was gripped by historic images
- of Apollo 11, and Neil Armstrong -- the first man to walk 
- on the moon. It was the height of the Cold War, and the charts
- were filled with David Bowie's Space Oddity, and Creedence's 
- Bad Moon Rising. The world is a very different place than 
- it was 5 decades ago. But how has the space race changed since
- the summer of '69? (Source: Bloomberg)
+of Apollo 11, and Neil Armstrong -- the first man to walk 
+on the moon. It was the height of the Cold War, and the charts
+were filled with David Bowie's Space Oddity, and Creedence's 
+Bad Moon Rising. The world is a very different place than 
+it was 5 decades ago. But how has the space race changed since
+the summer of '69? (Source: Bloomberg)
 
 
 Twitter CEO Jack Dorsey Gives Talk at Apple Headquarters
 
 Twitter and Square Chief Executive Officer Jack Dorsey 
- addressed Apple Inc. employees at the iPhone maker’s headquarters
- Tuesday, a signal of the strong ties between the Silicon Valley giants.
+addressed Apple Inc. employees at the iPhone maker’s headquarters
+Tuesday, a signal of the strong ties between the Silicon Valley giants.
 '''
+
+saved_pages = {
+    "bloomberg": bloomberg_com,
+    "nytimes": nytimes_com
+}
 
 
 def create_file(directory, filename, contents):
     file_path = os.path.join(directory, filename)
-    if not exists(file_path):
+    if not os.path.exists(file_path):
         with open(file_path, "w") as file:
             file.write(contents)
 
@@ -51,19 +54,19 @@ def create_directory(path):
         os.makedirs(path)
 
 
-
 def print_invalid_url():
-    print("Error: Incorrect URL")
+    print("Error: Invalid URL")
 
 
 def main():
     args = sys.argv
     directory = args[1]
-    # directory = "/home/myung/projects/hyperskill/python/Text-Based Browser/Text-Based Browser/task/tab"
     create_directory(directory)
     absolute_path = os.path.join(os.getcwd(), directory)
     while True:
-        user_input = input()
+        user_input = input().strip()
+        if user_input == "exit":
+            break
         if "." in user_input:
             if user_input == "bloomberg.com":
                 print(bloomberg_com)
@@ -73,22 +76,12 @@ def main():
                 create_file(absolute_path, "nytimes", nytimes_com)
             else:
                 print_invalid_url()
-        elif user_input == "bloomberg":
-            file = Path(os.path.join(absolute_path, user_input))
-            if file.is_file():
-                print(bloomberg_com)
-            else:
-                print_invalid_url()
-        elif user_input == "nytimes":
-            file = Path(os.path.join(absolute_path, user_input))
-            if file.is_file():
-                print(nytimes_com)
-            else:
-                print_invalid_url()
-        elif user_input == "exit":
-            break
         else:
-            print_invalid_url()
+            clean_input = user_input.split(".")[0]
+            if clean_input in saved_pages:
+                print(saved_pages[clean_input])
+            else:
+                print_invalid_url()
 
 
 if __name__ == "__main__":
