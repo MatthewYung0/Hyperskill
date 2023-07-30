21880dc07bba58dc5e10f1ffffd5f3de2bd91570
MatthewYung0 <65487408+MatthewYung0@users.noreply.github.com>
2023-07-30 17:57:08 +0200

    Create browser.py

---
+++ Python/3_Hard/TextBasedBrowser/5_6-Parsing/browser.py
@@ -0,0 +1,78 @@
+import sys
+import os
+from os.path import exists
+from bs4 import BeautifulSoup
+
+import requests
+
+
+def create_file(directory, filename, contents):
+    file_path = os.path.join(directory, filename)
+    if not os.path.exists(file_path):
+        with open(file_path, "w", encoding="utf=8") as file:
+            file.write(contents)
+
+
+def create_directory(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def get_human_readable_text(page_content):
+    soup = BeautifulSoup(page_content, "html.parser")
+    for tag in soup(['script', 'style']):
+        tag.decompose()  # Remove unwanted tags like script and style
+    return soup.get_text()
+
+
+def print_file(path):
+    with open(path, "r", encoding="utf=8") as file:
+        print(file.read())
+
+
+def print_invalid_url():
+    print("Error: Invalid URL")
+
+
+def save_to_stack(stack, page):
+    if page != "":
+        stack.append(page)
+
+
+def main():
+    args = sys.argv
+    directory = args[1]
+    # directory = "/home/myung/projects/hyperskill/python/Text-Based Browser/Text-Based Browser/task/tabs"
+    create_directory(directory)
+    absolute_path = os.path.join(os.getcwd(), directory)
+    current_page = ""
+    stack_ = []
+    while True:
+        user_input = input().strip()
+        clean_input = user_input.split(".")[0]
+        if user_input == "exit":
+            break
+        if "." in user_input:
+            req = requests.get(os.path.join("https://", user_input))
+            if req.status_code == 200:
+                text = get_human_readable_text(req.content)
+                save_to_stack(stack_, current_page)
+                create_file(absolute_path, clean_input, text)
+                current_page = text
+                print(text)
+            else:
+                print_invalid_url()
+        elif user_input == "back":
+            if stack_:
+                print(stack_.pop())
+        else:
+            if exists(os.path.join(absolute_path, clean_input)):
+                save_to_stack(stack_, current_page)
+                print_file(os.path.join(absolute_path, clean_input))
+                current_page = clean_input
+            else:
+                print_invalid_url()
+
+
+if __name__ == "__main__":
+    main()
