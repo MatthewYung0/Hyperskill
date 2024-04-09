5e5577c66b45f6d92ef00b1dbab395abad99a735
MatthewYung0 <65487408+MatthewYung0@users.noreply.github.com>
2024-04-09 14:09:25 +0200

    Stage 4
    Stage 4

--- Java/2_Medium/DesktopCalculator/Calculator.java
+++ Java/2_Medium/DesktopCalculator/Calculator.java
@@ -3,6 +3,8 @@ package calculator;
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator extends JFrame {
     private String problem;
@@ -14,10 +16,10 @@ public class Calculator extends JFrame {
     private static final char divisionSymbol = '\u00F7';
 
     private final String[][] BUTTON_TEXTS = {
-            {"7", "8", "9", "\u002B"},
-            {"4", "5", "6", "-"},
-            {"1", "2", "3", "\u00D7"},
-            {"0", ".", "=", "\u00F7"},
+            {"7", "8", "9", String.valueOf(additionSymbol)},
+            {"4", "5", "6", String.valueOf(subtractionSymbol)},
+            {"1", "2", "3", String.valueOf(multiplicationSymbol)},
+            {"0", ".", "=", String.valueOf(divisionSymbol)},
             {"C", "Del"}
     };
 
@@ -53,22 +55,37 @@ public class Calculator extends JFrame {
                 switch (BUTTON_TEXTS[row][column]) {
                     // If button is "=", execute the calculate function
                     case "=" -> button.addActionListener(e -> {
-                        problem = equationTextField.getText();
-                        ArrayList<String> infixExpression = infixParse(problem);
-                        ArrayList<String> postfixExpression = infixToPostfixConvertor(infixExpression);
-                        answer = calculatePostfixExpression(postfixExpression);
-                        resultTextField.setText(answer);
+                        String problem = equationTextField.getText();
+                        if (checkEquation(problem)) {
+                            ArrayList<String> infixExpression = infixParse(problem);
+                            ArrayList<String> postfixExpression = infixToPostfixConvertor(infixExpression);
+                            answer = calculatePostfixExpression(postfixExpression);
+                            resultTextField.setText(answer);
+                        } else {
+                            equationTextField.setForeground(Color.RED.darker());
+                        }
                     });
                     // else if button is "C", clear the text
                     case "C" -> button.addActionListener(e -> equationTextField.setText(""));
-                    // else if button is "Del", delete the previous
+                    // else if button is "Del", delete the previous character
                     case "Del" -> button.addActionListener(e -> {
                         String currentText = equationTextField.getText();
-                        String newText = currentText.substring(0, currentText.length()-1);
+                        String newText = currentText.substring(0, currentText.length() - 1);
                         equationTextField.setText(newText);
+                    });
+                    // For operator buttons, including formatting logic
+                    case "\u002B", "-", "\u00D7", "\u00F7" -> button.addActionListener(e -> {
+                        JButton clickedButton = (JButton) e.getSource();
+                        String currentEquation = equationTextField.getText() + clickedButton.getText();
+                        if (currentEquation.length() != 1) {
+                            String newEquation = formatEquation(currentEquation);
+                            equationTextField.setForeground(Color.BLACK);
+                            equationTextField.setText(newEquation);
+                        }
                     });
                     // otherwise print the value of the button to the text instead
                     default -> button.addActionListener(e -> {
+                        equationTextField.setForeground(Color.BLACK);
                         JButton clickedButton = (JButton) e.getSource();
                         String buttonText = clickedButton.getText();
                         String currentEquation = equationTextField.getText();
@@ -80,6 +97,49 @@ public class Calculator extends JFrame {
             }
         }
         return buttonPanel;
+    }
+
+    private String formatEquation(String problem) {
+        String newEquation = formatOperator(problem);
+        newEquation = formatZeros(newEquation);
+        return newEquation;
+    }
+
+    // If 2x operators are typed in sequentially, replace the first operator with the second operator
+    private String formatOperator(String problem) {
+        if (problem.length() > 2) {
+            char lastChar = problem.charAt(problem.length() - 1);
+            char secondToLastChar = problem.charAt(problem.length() - 2);
+            if (lastChar == additionSymbol ||
+                    lastChar == subtractionSymbol ||
+                    lastChar == multiplicationSymbol ||
+                    lastChar == divisionSymbol) {
+                if (secondToLastChar == additionSymbol ||
+                        secondToLastChar == subtractionSymbol ||
+                        secondToLastChar == multiplicationSymbol ||
+                        secondToLastChar == divisionSymbol) {
+                    StringBuilder newEquation = new StringBuilder(problem);
+                    newEquation.deleteCharAt(problem.length() - 2);
+                    return newEquation.toString();
+                }
+            }
+        }
+        return problem;
+    }
+
+    // Adds 0 if the number entered is .0 or 0. as an example.
+    private String formatZeros(String problem) {
+        if (problem.contains(".")) {
+            int indexOfDot = problem.lastIndexOf(".");
+            StringBuilder newEquation = new StringBuilder(problem);
+            if (indexOfDot == 0) {
+                newEquation.insert(indexOfDot, '0');
+            } else if (indexOfDot == problem.length() - 2) {
+                newEquation.insert(problem.length() - 1, '0');
+            }
+            return newEquation.toString();
+        }
+        return problem;
     }
 
     private JPanel createMainPanel(JLabel equationTextField, JLabel resultTextField, JPanel buttonPanel) {
@@ -100,6 +160,25 @@ public class Calculator extends JFrame {
         JLabel resultTextField = new JLabel();
         resultTextField.setName("ResultLabel");
         return resultTextField;
+    }
+
+    private boolean checkEquation(String problem) {
+        if (problem.length() < 3) {
+            return false;
+        }
+        char[] problemAsCharArr = problem.toCharArray();
+        if (!Character.isDigit(problemAsCharArr[0])) {
+            return false;
+        }
+        int lastIndex = problemAsCharArr.length - 1;
+        if (!Character.isDigit(problemAsCharArr[lastIndex])) {
+            return false;
+        }
+        if (problemAsCharArr[lastIndex - 1] == '\u00F7' && problemAsCharArr[lastIndex] == '0') {
+            return false;
+        }
+        return true;
+
     }
 
     private ArrayList<String> infixParse(String data) {
